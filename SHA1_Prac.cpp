5f7d6c44466f04a73663e0213a5bdf3c078b95e7
Lord_Hypersonic <jatin.bal16@gmail.com>
2021-01-17 20:03:42 +0530

    dividing each character of message into 512 chunks

--- SHA1_Prac.cpp
+++ SHA1_Prac.cpp
@@ -27,8 +27,23 @@ string paddingBinarywithZero_448Length(string BinaryMessage)
 int main()
 {
     string message = "A Test", binary_of_message = "";
-    binary_of_message = ToBinary(message);
+    string BinaryOfMessageCharacters[message.size()]; //binary of each character in message
+    for (int i = 0; i < message.size(); i++)//splitting characters of message and converting into binary
+    {
+        string TempMessageCharacter;
+        TempMessageCharacter = message[i];
+        BinaryOfMessageCharacters[i] = ToBinary(TempMessageCharacter);
+        cout<<BinaryOfMessageCharacters[i]<<endl;
+    }
+    binary_of_message = ToBinary(message); //finding binary of message
     binary_of_message = paddingBinarywithZero_448Length(binary_of_message);
-    cout<<binary_of_message<<endl<<binary_of_message.size()<<endl;
+    string _512MessageChunks[message.size()]; //array of chunk of 512 characters
+    for (int i = 0; i < message.size(); i++) //calculating chunk for each character in message
+    {
+        string TempMessageCharacterBinary = BinaryOfMessageCharacters[i];
+        while (TempMessageCharacterBinary.size() != 64) //making binary of each character to 64-bit by adding 0 in front of previous binary
+            TempMessageCharacterBinary = "0" + TempMessageCharacterBinary;
+        _512MessageChunks[i] = binary_of_message + TempMessageCharacterBinary;
+    }
     return 0;
 }
