f7d1a80eb2f4f84d4429998a176bedd7ea6dc3f0
Lord_Hypersonic <jatin.bal16@gmail.com>
2021-01-17 19:15:49 +0530

    pad until length is 512 mod 448 with functions

--- SHA1_Prac.cpp
+++ SHA1_Prac.cpp
@@ -4,16 +4,31 @@
 
 using namespace std;
 
+//function to convert a string to binary digits of 8-bit
+string ToBinary(string message)
+{
+    string binary_of_message = "";
+    for (int i = 0; i < message.size(); i++)
+        binary_of_message += bitset<8>(message[i]).to_string();
+    return binary_of_message;
+}
+
+//function to set up binary digits upto pad the binary message with zeros until its length is 512 % 448
+string paddingBinarywithZero_448Length(string BinaryMessage)
+{
+    BinaryMessage.push_back('1');
+    while (BinaryMessage.size()%512 != 448)
+    {
+        BinaryMessage.push_back('0');
+    }
+    return BinaryMessage;
+}
+
 int main()
 {
     string message = "A Test", binary_of_message = "";
-    for (int i = 0; i < message.size(); i++)
-        binary_of_message += bitset<8>(message[i]).to_string();
-    binary_of_message.push_back('1');
-    while (binary_of_message.size()%512 != 448)
-    {
-        binary_of_message.push_back('0');
-    }
-    cout<<binary_of_message<<endl;
+    binary_of_message = ToBinary(message);
+    binary_of_message = paddingBinarywithZero_448Length(binary_of_message);
+    cout<<binary_of_message<<endl<<binary_of_message.size()<<endl;
     return 0;
 }
