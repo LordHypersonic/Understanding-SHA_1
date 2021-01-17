5f8e7106d00c1c30a222c63d183a6847edafce80
Lord_Hypersonic <jatin.bal16@gmail.com>
2021-01-17 21:48:54 +0530

    Revert "dividing each character of message into 512 chunksdividing each character of message into 512 chunks (corrected)"
    This reverts commit a226b2dd297a1766979b99600e4a0f08fbd0ce35.

--- SHA1_Prac.cpp
+++ SHA1_Prac.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <string>
 #include <bitset>
-#include <algorithm>
 
 using namespace std;
 
 //function to convert a string to binary digits of 8-bit
-string StringtoBinary(string message)
+string ToBinary(string message)
 {
     string binary_of_message = "";
     for (int i = 0; i < message.size(); i++)
@@ -14,32 +13,11 @@ string StringtoBinary(string message)
     return binary_of_message;
 }
 
-//function to convert integer to binary
-string IntegertoBinary(int number)
-{
-    string BinaryDigit = "";
-    while(number != 0)
-    {
-        if (number % 2 == 0)
-        {
-            number = number / 2;
-            BinaryDigit.push_back('0');
-        }
-        else
-        {
-            number = number / 2;
-            BinaryDigit.push_back('1');
-        }
-    }
-    reverse(BinaryDigit.begin(), BinaryDigit.end());
-    return BinaryDigit;
-}
-
 //function to set up binary digits upto pad the binary message with zeros until its length is 512 % 448
 string paddingBinarywithZero_448Length(string BinaryMessage)
 {
     BinaryMessage.push_back('1');
-    while (BinaryMessage.size()%512 != 448) //adding zeros until length become 448
+    while (BinaryMessage.size()%512 != 448)
     {
         BinaryMessage.push_back('0');
     }
@@ -54,26 +32,18 @@ int main()
     {
         string TempMessageCharacter;
         TempMessageCharacter = message[i];
-        BinaryOfMessageCharacters[i] = StringtoBinary(TempMessageCharacter);
+        BinaryOfMessageCharacters[i] = ToBinary(TempMessageCharacter);
         cout<<BinaryOfMessageCharacters[i]<<endl;
     }
-    binary_of_message = StringtoBinary(message); //finding binary of message
-    string binaryAfterAdding448_0 = paddingBinarywithZero_448Length(binary_of_message);
-    string BinaryOfBinaryMessageLength = IntegertoBinary(binary_of_message.size()); // finding binary of length of binary of message.
-    while (BinaryOfBinaryMessageLength.size() != 64) //adding 0 in front of binary of binary of message until its length become 64
+    binary_of_message = ToBinary(message); //finding binary of message
+    binary_of_message = paddingBinarywithZero_448Length(binary_of_message);
+    string _512MessageChunks[message.size()]; //array of chunk of 512 characters
+    for (int i = 0; i < message.size(); i++) //calculating chunk for each character in message
     {
-        BinaryOfBinaryMessageLength = "0" + BinaryOfBinaryMessageLength;
-    }
-    cout<<BinaryOfBinaryMessageLength<<endl<<endl;
-    string Adding448StringWith64string = binaryAfterAdding448_0 + BinaryOfBinaryMessageLength;
-    string _512MessageChunks[Adding448StringWith64string.size() / 512]; //array of chunk of 512 characters
-    int pos = 0;
-    for (int i = 0; i < Adding448StringWith64string.size() / 512; i++)//breaking previous result into chunks of 512 bits
-    {
-
-        _512MessageChunks[i] = Adding448StringWith64string.substr(pos, 512);
-        cout<<_512MessageChunks[i]<<endl;
-        pos += 512;
+        string TempMessageCharacterBinary = BinaryOfMessageCharacters[i];
+        while (TempMessageCharacterBinary.size() != 64) //making binary of each character to 64-bit by adding 0 in front of previous binary
+            TempMessageCharacterBinary = "0" + TempMessageCharacterBinary;
+        _512MessageChunks[i] = binary_of_message + TempMessageCharacterBinary;
     }
     return 0;
 }
