9fbdcf021c9198e72374eac0cc4784701b1a2243
Lord_Hypersonic <jatin.bal16@gmail.com>
2021-01-18 18:19:28 +0530

    almost done but with wrong output

--- SHA1_Prac.cpp
+++ SHA1_Prac.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <bitset>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
@@ -80,7 +81,7 @@ string AND (string binary1,string binary2)
     string output = "";
     for (int i = 0; i < binary1.size(); i++)
     {
-        if (binary1[i] == binary2[i])
+        if (binary1[i] == '1' && binary2[i] == '1')
             output.push_back('1');
         else output.push_back('0');
     }
@@ -115,6 +116,16 @@ string NOT(string binaryString)
 string BinaryAddition(string a, string b)
 {
     string result = ""; // Initialize result
+    /*string Carry =  "0";
+    for (int i = 0; i < a.size(); i++)
+    {
+        string TempA, TempB, TempResult;
+        TempA = a[i]; TempB = b[i];
+        TempResult = XOR ( XOR(TempA,TempB), Carry);
+        Carry = OR( AND(XOR(TempA,TempB),Carry),AND(TempA,TempB));
+        //cout<<Carry<<endl;
+        result += TempResult;
+    }*/
     int s = 0;          // Initialize digit sum
 
     // Traverse both strings starting from last
@@ -122,7 +133,7 @@ string BinaryAddition(string a, string b)
     int i = a.size() - 1, j = b.size() - 1;
     while (i >= 0 || j >= 0 || s == 1)
     {
-        // Comput sum of last digits and carry
+        // Compute sum of last digits and carry
         s += ((i >= 0)? a[i] - '0': 0);
         s += ((j >= 0)? b[j] - '0': 0);
 
@@ -136,6 +147,35 @@ string BinaryAddition(string a, string b)
         i--; j--;
     }
     return result;
+}
+
+//function to convert binary string to hex string
+string Binary_to_Hex(string BinaryString)
+{
+    string output;
+    map <string, string> hexValues;
+    hexValues.insert(pair<string,string>("0000","0"));
+    hexValues.insert(pair<string,string>("0001","1"));
+    hexValues.insert(pair<string,string>("0010","2"));
+    hexValues.insert(pair<string,string>("0011","3"));
+    hexValues.insert(pair<string,string>("0100","4"));
+    hexValues.insert(pair<string,string>("0101","5"));
+    hexValues.insert(pair<string,string>("0110","6"));
+    hexValues.insert(pair<string,string>("0111","7"));
+    hexValues.insert(pair<string,string>("1000","8"));
+    hexValues.insert(pair<string,string>("1001","9"));
+    hexValues.insert(pair<string,string>("1010","a"));
+    hexValues.insert(pair<string,string>("1011","b"));
+    hexValues.insert(pair<string,string>("1100","c"));
+    hexValues.insert(pair<string,string>("1101","d"));
+    hexValues.insert(pair<string,string>("1110","e"));
+    hexValues.insert(pair<string,string>("1111","f"));
+    for(int i = 0; i < BinaryString.size(); i += 4)
+    {
+        string temp = BinaryString.substr(i, 4);
+        output = output + hexValues[temp];
+    }
+    return output;
 }
 
 int main()
@@ -193,7 +233,7 @@ int main()
            h3 = "00010000001100100101010001110110",
            h4 = "11000011110100101110000111110000";
     string a = h0, b = h1, c = h2, d = h3, e = h4;
-    /*//for calculating final hash by manipulating the h0, h1, h2, h3 and h4 using 32 bit words calculated previously
+    //for calculating final hash by manipulating the h0, h1, h2, h3 and h4 using 32 bit words calculated previously
     for (int row = 0; row < Adding448StringWith64string.size() / 512; row++)
     {
         for(int column = 0; column < 80; column++)
@@ -220,19 +260,22 @@ int main()
                 k = "11001010011000101100000111010110";
             }
             string temp = BinaryAddition( BinaryAddition( BinaryAddition( BinaryAddition( LeftRotate(a,5),f),e),k),_32BitChunks[row][column]);
+            temp.resize(32);
             e = d;
             d = c;
             c = LeftRotate(b, 30);
             b = a;
             a = temp;
+            //cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<endl;
         }
-        h0 = BinaryAddition(h0,a);
-        h1 = BinaryAddition(h1,b);
-        h2 = BinaryAddition(h2,c);
-        h3 = BinaryAddition(h3,d);
-        h4 = BinaryAddition(h4,e);
+        h0 = BinaryAddition(h0,a); h0.resize(32);
+        h1 = BinaryAddition(h1,b); h1.resize(32);
+        h2 = BinaryAddition(h2,c); h2.resize(32);
+        h3 = BinaryAddition(h3,d); h3.resize(32);
+        h4 = BinaryAddition(h4,e); h4.resize(32);
     }
-    cout<<h0<<endl<<h1<<endl<<h2<<endl<<h3<<endl<<h4<<endl;*/
-    cout<<BinaryAddition("00111", "11010");
+    cout<<h0<<endl<<h1<<endl<<h2<<endl<<h3<<endl<<h4<<endl;
+    string FinalHash = Binary_to_Hex(h0+h1+h2+h3+h4);
+    cout<<FinalHash<<endl;
     return 0;
 }
